26082df23e79621a25baf9d342b5dc63bcdfc4dd
diatmpravin <diatm.pravin.it.07.27@gmail.com>
2012-11-10 17:42:52 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultipleRow::Application.config.secret_token = '77b9dc59173ea461505ce4664055c08b10c5819abff1f2b90eea5658b6c0c4f5b632e1be5e409d982135bde2a08b13d15b81b4a7978227d041da646a67210488'
